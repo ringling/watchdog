a489cdcf74e4f301456bfaeb122ce2fa31368d6a
Thomas Ringling <thomas.ringling@gmail.com>
2016-08-09 14:04:16 +0200

    Quantum dependency

--- mix.exs
+++ mix.exs
@@ -10,23 +10,13 @@ defmodule DeadMansSwitch.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :quantum]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:quantum, ">= 1.7.1"}
+    ]
   end
 end
